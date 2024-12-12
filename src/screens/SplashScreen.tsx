47b43e0075625fa913cd3086c45ccdcbdfa4dce0
SyQy10 <130484427+SyQuy27683@users.noreply.github.com>
2024-12-12 19:39:36 +0700

    add SplashScreen done

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { ActivityIndicator, Image, ImageBackground, View } from 'react-native';
+import { appInfor } from '../constants/appInfor';
+import { SpaceComponent } from '../components';
+import { appColor } from '../constants/appColor';
+
+
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
-  )
+    <ImageBackground
+      source={require('../assets/images/splash-img.png')}
+      style={{
+        flex: 1, 
+        justifyContent:'center',
+        alignItems: 'center',}}
+        imageStyle={{flex: 1}}>
+
+      <Image source={require('../assets/images/logo.png')} 
+      style={{
+        width: appInfor.sizes.WIDTH * 0.7,
+        resizeMode: 'contain',
+      }}/>
+
+      <SpaceComponent height={20} />
+        <ActivityIndicator color={appColor.gray} size={22} />
+
+    </ImageBackground>
+    
+  );
 }
 
 export default SplashScreen
