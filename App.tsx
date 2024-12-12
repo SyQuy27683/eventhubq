95e791a6c50d8342fffb08090111345bfa8dadd6
SyQy10 <130484427+SyQuy27683@users.noreply.github.com>
2024-12-12 20:09:05 +0700

    P2 user navigation done

--- App.tsx
+++ App.tsx
@@ -3,17 +3,38 @@ import React, { useEffect, useState } from 'react'
 import { SplashScreen } from './src/screens';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './src/navigators/AuthNavigator';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
+import MainNavigator from './src/navigators/MainNavigator';
 
 const App = () => {
 
+  // State
   const [isShowSplash, setIsShowSplash] = useState(true);
 
+  const [assetToken, setAssetToken] = useState('');
+
+  const {getItem, setItem} = useAsyncStorage('assetToken');
+
+// useEffect
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsShowSplash(false);
     },1500);
     return () => clearTimeout(timeout);
   }, []);
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+
+  // Function
+  const checkLogin = async () => {
+    const token = await getItem();
+    
+    token && setAssetToken(token);
+  }
+
 
   return (
   <>
@@ -25,7 +46,7 @@ const App = () => {
       <SplashScreen/> 
         ) : (
           <NavigationContainer>
-            <AuthNavigator/>
+            {assetToken ? <MainNavigator/> : <AuthNavigator/>}
           </NavigationContainer>
         )}
         </>
